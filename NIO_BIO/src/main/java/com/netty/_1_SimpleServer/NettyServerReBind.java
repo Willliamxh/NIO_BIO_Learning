44544bbf6419828570d8823a45c2c76eedb05a60
xuhan <xuhan.xuhan@alibaba-inc.com>
2023-11-15 15:21:32 +0800

    Netty入门程序解析： 7.1.服务端相关代码参数解析

--- NIO_BIO/src/main/java/com/netty/_1_SimpleServer/NettyServerReBind.java
+++ NIO_BIO/src/main/java/com/netty/_1_SimpleServer/NettyServerReBind.java
@@ -6,6 +6,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
 
 /**
  * @author XuHan
@@ -27,7 +28,22 @@ public class NettyServerReBind {
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap
                 .group(boosGroup, workerGroup)
-                .option(ChannelOption.SO_REUSEADDR, true)//
+                // 指定自定义属性，客户端可以根据此属性进行一些判断处理
+                // 可以看作给Channel维护一个Map属性，这里的channel是服务端
+                // 允许指定一个新创建的通道的初始属性。如果该值为空，指定键的属性将被删除。
+                .attr(AttributeKey.newInstance("hello"), "hello world")
+                // 给每个连接指定自定义属性，Channel 进行属性指定等
+                // 用给定的值在每个 子通道 上设置特定的AttributeKey。如果该值为空，则AttributeKey将被删除。
+                // 区别是是否是 子channel，子Channel代表给客户端的连接设置
+                .childAttr(AttributeKey.newInstance("childAttr"), "childAttr")
+                // 客户端的 Channel 设置TCP 参数
+                // so_backlog 临时存放已完成三次握手的请求队列的最大长度，如果频繁连接可以调大此参数
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                // 给每个连接设置TCP参数
+                // tcp的心跳检测，true为开启
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                // nagle 算法开关，实时性要求高就关闭
+                .childOption(ChannelOption.TCP_NODELAY, true)
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
